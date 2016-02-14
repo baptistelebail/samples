2168e80275b0ad4bdef0a44edf1aad450d5fc6df
Baptiste Le Bail <baptiste.lebail@linuxmail.org>
2016-02-14 02:07:18 +0100

    Update gulpfile.js

--- gulp-include-file/gulpfile.js
+++ gulp-include-file/gulpfile.js
@@ -1,11 +1,10 @@
 'use-strict';
 
 const gulp = require('gulp'),
-    gutil = require('gulp-util'),
     config = require('./config');
 
 gulp.task('default', function() {
-    gutil.log(config.paths.app);
-    gutil.log(config.paths.assets);
-    gutil.log(config.paths.build);
+    console.log(config.paths.app);
+    console.log(config.paths.assets);
+    console.log(config.paths.build);
 });

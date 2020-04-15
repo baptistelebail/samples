5c3445ad8770f4586a7f100c8a6908ed33ea4533
baptistelebail <blebail@protonmail.com>
2020-04-15 17:11:11 +0200

    (post) java-sanitize-html add tests

--- java-sanitize-html/src/test/java/com/blebail/blog/sample/sanitize/HtmlSanitizationPolicyTest.java
+++ java-sanitize-html/src/test/java/com/blebail/blog/sample/sanitize/HtmlSanitizationPolicyTest.java
@@ -18,6 +18,16 @@ public final class HtmlSanitizationPolicyTest {
         assertThat(sanitized).isEqualTo("Text with a link and");
     }
 
+    @Test
+    public void shouldNotAllowJavaScriptOnArticlePolicy() {
+        String text = "Text with <a href=\"https://example.com\" rel=\"nofollow\">a link</a> " +
+                "and<script>alert('javascript');</script>";
+
+        String sanitized = HtmlSanitizationPolicy.ARTICLE.sanitize(text);
+
+        assertThat(sanitized).isEqualTo("Text with <a href=\"https://example.com\" rel=\"nofollow\">a link</a> and");
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {
             "A <h1>Title</h1> and a <p>paragraph</p>",
@@ -30,6 +40,16 @@ public final class HtmlSanitizationPolicyTest {
         String sanitized = HtmlSanitizationPolicy.ARTICLE.sanitize(text);
 
         assertThat(sanitized).isEqualTo(text);
+    }
+
+    @Test
+    public void shouldNotAllowLinksOrJavaScriptOnCustomPolicy() {
+        String text = "Text with <a href=\"https://example.com\" rel=\"nofollow\">a link</a> " +
+                "and<script>alert('javascript');</script> and <my-element>Mine</my-element>";
+
+        String sanitized = HtmlSanitizationPolicy.CUSTOM.sanitize(text);
+
+        assertThat(sanitized).isEqualTo("Text with a link and and <my-element>Mine</my-element>");
     }
 
     @Test
